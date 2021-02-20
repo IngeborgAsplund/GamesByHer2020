73729e4c03705526f8ecd47ee814037fc7dd7ba4
Hortencya <ingeborg.asplund@gmail.com>
2021-02-20 16:20:53 +0100

    AI started

--- SFMLTemplate/source/aiShip.cpp
+++ SFMLTemplate/source/aiShip.cpp
@@ -1,9 +1,42 @@
+#include"sfml-engine/mathutils.h"
+
+
 #include"aiShip.h"
+
+//Give acess to the checkpoints we have in scene and assign the current checkpoint to the first one 
+void AiControlledShip::AssignListOfCheckpoints(std::vector<std::shared_ptr<CheckPoint>> inCheckpoint)
+{
+	checkpointsInScene = inCheckpoint;
+	checkIndex = 0;
+	currentCheckpoint = checkpointsInScene[checkIndex];
+	reached = false;
+}
 
 void AiControlledShip::FindNewCeckPoint()
 {
-
+	if(checkIndex>-1&&checkIndex< checkpointsInScene.size()-1)
+	{
+		checkIndex++;
+		currentCheckpoint = checkpointsInScene[checkIndex];
+		reached = false;
+	}
 }
 void AiControlledShip::MoveTowerds()
 {
+	float xDirection = currentCheckpoint->getPosition().x-this->getPosition().x;
+	float yDirection = currentCheckpoint->getPosition().y - this->getPosition().y;
+	direction = gbh::math::normalize(const sf::Vector2f(xDirection, yDirection));
+	if(!reached)
+	{
+		getPhysicsBody()->applyForceToCenter(direction*speed);
+
+		if (this->getPosition() == currentCheckpoint->getPosition())
+		{
+			reached = true;
+		}
+	}
+	else if(reached)
+	{
+		FindNewCeckPoint();
+	}
 }
--- SFMLTemplate/source/aiShip.h
+++ SFMLTemplate/source/aiShip.h
@@ -1,4 +1,5 @@
 #pragma once
+#include<vector>
 #include"sfml-engine/spritenode.h"
 #include"sfml-engine/physics/physicsbody.h"
 
@@ -8,11 +9,15 @@ class AiControlledShip:public gbh::SpriteNode
 {	
 public:
 	void MoveTowerds();
+	void AssignListOfCheckpoints(std::vector<std::shared_ptr<CheckPoint>> inCheckpoint);
 private:
 	//variables
 	std::shared_ptr<CheckPoint> currentCheckpoint;
+	sf::Vector2f direction;
     float speed = 4.0f;//speed we use as force for the enemy ship/agent
+	int checkIndex = -1;
 	bool reached = false;
+	std::vector<std::shared_ptr<CheckPoint>> checkpointsInScene;
 	//functions
 	void FindNewCeckPoint();
 };
